cff2f163fc0ea42fcc738cd9ad2dbb59c2f1cca7
Wanda Schneid <schnewan@students.zhaw.zh>
2025-02-26 08:37:29 +0100

    Mailadresse  geändert

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -8,10 +8,8 @@ st.title("Meine erste Streamlit App")
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Wanda Schneid (schnewan@students.zhaw.ch)
+- Riccardo Reich (reichri1@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-
-Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
